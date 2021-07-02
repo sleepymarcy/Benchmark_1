2f530fb75ae8e136631d599f9282efcc4a426b37
Martyna <martyna.sowinska@hotmail.com>
2021-07-02 17:06:44 +0200

    DOM Part 1

--- main.js
+++ main.js
@@ -11,20 +11,6 @@
     28) Create an array of arrays, in which every array has 10 random numbers +
     29) Create a function that gets 2 arrays and returns the longest one +
     30) Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values +
-
-    DOM 
-
-    31) Get element with ID "container" from the page
-    32) Get every "td" from the page
-    33) Create a cycle that prints the text inside every td of the page
-    34) Write a function to change the heading of the page
-    35) Write a function to add an extra row to the table
-    36) Write a function to add the class "test" to each row in the table
-    37) Write a function to add a red background to every link in the page
-    38) Console log "Page loaded" when the page is correctly loaded
-    39) Write a function to add new items to a UL
-    40) Write a function to empty a list
-    
 */
 
 let x = "John"
@@ -111,3 +97,23 @@ function higherSumOfEl(arr1, arr2){
 }
 
 // console.log(higherSumOfEl([1, 1, 1], [1, 2, 3]))
+
+/*
+    DOM 
+
+    31) Get element with ID "container" from the page
+    32) Get every "td" from the page
+    33) Create a cycle that prints the text inside every td of the page
+    34) Write a function to change the heading of the page
+    35) Write a function to add an extra row to the table
+    36) Write a function to add the class "test" to each row in the table
+    37) Write a function to add a red background to every link in the page
+    38) Console log "Page loaded" when the page is correctly loaded
+    39) Write a function to add new items to a UL
+    40) Write a function to empty a list
+*/
+
+document.getElementById('container')
+
+document.getAttribute('td')
+
